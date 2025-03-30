ba7df55d3c6a749c5cdcb542f16882896fa8cff0
liloskiller <70860217+liloskiller@users.noreply.github.com>
2025-03-30 19:50:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.post('/signup', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server running on port 3000'));
