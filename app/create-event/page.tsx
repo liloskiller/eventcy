5f06ea8e59cc8f09d4794b150f51df1afe95b1d4
liloskiller <kostasth@outlook.com>
2025-04-28 22:51:23 +0300

    create-event imp

--- app/create-event/page.tsx
+++ app/create-event/page.tsx
@@ -7,7 +7,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Switch } from '@/components/ui/switch'
-import BackToMain from "@/components/BackButton"
+import { BackToMain }  from "@/components/BackButton"
 
 export default function CreateEventPage() {
   const { user } = useAuth()
@@ -79,7 +79,7 @@ export default function CreateEventPage() {
 
   return (
     //<div className="min-h-screen bg-gradient-to-br from-purple-600 to-blue-600 p-4 sm:p-6 font-sans"></div>
-    <div className="min-h-screen bg-gradient-to-br from-purple-600 to-blue-600 font-sans">
+    <div className="min-h-screen bg-gradient-to-br from-purple-600 to-blue-600 flex items-center justify-center p-6 font-sans relative">
       <BackToMain />
       <Card className="w-full max-w-2xl bg-white shadow-lg rounded-2xl p-6">
         <CardHeader className="text-center">
